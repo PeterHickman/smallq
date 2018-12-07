9196821499bd87099f6c19d5d08983be6d5f6615
Peter Hickman <peterhickman386@googlemail.com>
2018-12-07 14:16:08 +0000

    a progress bar of sorts

--- client_add.rb
+++ client_add.rb
@@ -2,6 +2,24 @@
 
 $LOAD_PATH << './lib'
 
+class Progress
+  def initialize(minimum, maximum)
+    @minimum = minimum
+    @maximum = maximum
+
+    @size = @maximum - @minimum + 1
+ 	@current = 0
+  end
+
+  def draw
+    print "Processing %.2f%% of %d to %d          \r" % [(@current.to_f / @size.to_f) * 100.0, @minimum, @maximum]
+  end
+
+  def inc
+    @current += 1 unless @current == @size
+  end
+end
+
 require 'smallq/client'
 
 number_of_messages = ARGV[0].to_i
@@ -10,12 +28,15 @@ TEST_MESSAGE = 'Test message'
 QUEUE = 'test_queue'
 
 c = Smallq::Client.new('localhost', 2000)
-
+p = Progress.new(1, number_of_messages)
 t1 = Time.now
 
 number_of_messages.times do
   c.add(QUEUE, TEST_MESSAGE)
+  p.inc
+  p.draw
 end
+puts
 
 t2 = Time.now
 
