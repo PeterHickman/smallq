bdb1d45d91f76fd94c538429f1853a926e118ccf
Peter Hickman <peterhickman386@googlemail.com>
2019-03-17 10:35:08 +0000

    new command and keep the connection open

--- lib/smallq/server.rb
+++ lib/smallq/server.rb
@@ -22,29 +22,38 @@ module Smallq
 
       loop do
         Thread.start(server.accept) do |client|
-          begin
-            m = client.gets.chomp.split(' ', 3)
+          loop do
+            begin
+              s = client.gets
+              next if s.nil?
+              m = s.chomp.split(' ', 3)
 
-            case m[0]
-            when 'ADD'
-              i = qm.add(m[1], m[2])
-              both(client, 'ADD', "OK #{i}")
-            when 'GET'
-              r = qm.get(m[1])
-              if r
-                both(client, 'GET', "OK #{r[MESSAGE_ID]} #{r[MESSAGE_BODY]}")
+              case m[0]
+              when 'ADD'
+                i = qm.add(m[1], m[2])
+                both(client, 'ADD', "OK #{i}")
+              when 'GET'
+                r = qm.get(m[1])
+                if r
+                  both(client, 'GET', "OK #{r[MESSAGE_ID]} #{r[MESSAGE_BODY]}")
+                else
+                  both(client, 'GET', 'ERROR QUEUE EMPTY')
+                end
+              when 'STATS'
+                qm.stats.each do |l|
+                  both(client, 'STATS', l.join(' '))
+                end
+                both(client, 'STATS', 'OK')
+              when 'QUIT'
+                break
               else
-                both(client, 'GET', 'ERROR QUEUE EMPTY')
+                both(client, 'STATS', 'ERROR UNKNOWN COMMAND')
               end
-            when 'STATS'
-              qm.stats.each do |l|
-                both(client, 'STATS', l.join(' '))
-              end
-            else
-              both(client, 'STATS', 'ERROR UNKNOWN COMMAND')
+            rescue => e
+              both(client, 'ERROR', "ERROR #{e}")
+              e.backtrace.each { |line| puts line }
+              break
             end
-          rescue => e
-            both(client, 'ERROR', "ERROR #{e}")
           end
 
           client.close
