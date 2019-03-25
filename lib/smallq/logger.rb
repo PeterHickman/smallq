ade8f0c0e57c9beceb28e10765a9bff0d4fe2343
Peter Hickman <peter@abelsoninfo.com>
2019-03-25 16:41:31 +0000

    proper logging

--- lib/smallq/logger.rb
+++ lib/smallq/logger.rb
@@ -1,13 +1,22 @@
-require 'logger'
-
 module Smallq
   class Logger
     def initialize(config)
-      @logger = ::Logger.new(STDERR)
+      @console = config['console']
+
+      @logger = nil
+      if config['enabled'] == true
+        @logger = File.open(config['path'], 'a')
+        @logger.sync = true
+      end
     end
 
-    def info(message)
-      @logger.info message
+    def log(message)
+      return if @logger.nil? && !@console
+
+      text = "#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%6N')} #{message}"
+
+      @logger.puts text if @logger
+      puts text if @console
     end
   end
 end
