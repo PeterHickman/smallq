8224794c51870b49e4a727d6513f62710a07ebf9
Peter Hickman <peterhickman386@googlemail.com>
2019-04-27 15:52:54 +0100

    produce the correct number of work units

--- example/primes/make_work.rb
+++ example/primes/make_work.rb
@@ -23,47 +23,51 @@ def queue_details(c)
   {}
 end
 
-limit = ARGV[1]
-if limit
-  limit = limit.to_i
-else
-  limit = 30
-end
-
 filename = ARGV[0]
 
 config = Smallq::Config.load(filename)
 
 logger = Logger.new(STDERR)
 
-logger.info "Work limit is #{limit}"
+limit = ARGV[1]
+if limit
+  limit = limit.to_i
+  logger.info "Work limit is #{limit}"
+else
+  limit = 0
+  logger.info "Work limit is unlimited"
+end
 
 c = Smallq::Client.new(config['server'])
 
 f = 1
 
-loop do
-  d = queue_details(c)
+if limit.zero?
+  loop do
+    d = queue_details(c)
 
-  size = d[:size] || 0
+    size = d[:size] || 0
 
-  logger.info "Queue #{QUEUE_NAME} size = #{size}"
+    logger.info "Queue #{QUEUE_NAME} size = #{size}"
 
-  if size < MINIMUM_QUEUE_SIZE
-    logger.info "Adding #{MINIMUM_QUEUE_SIZE * ALLOCATION_MULTIPLIER} new items"
+    if size < MINIMUM_QUEUE_SIZE
+      logger.info "Adding #{MINIMUM_QUEUE_SIZE * ALLOCATION_MULTIPLIER} new items"
 
-    (MINIMUM_QUEUE_SIZE * ALLOCATION_MULTIPLIER).times do
-      t = f + STEP
-      c.add(QUEUE_NAME, "#{f} #{t}")
-      f = t + 1
+      (MINIMUM_QUEUE_SIZE * ALLOCATION_MULTIPLIER).times do
+        t = f + STEP
+        c.add(QUEUE_NAME, "#{f} #{t}")
+        f = t + 1
+      end
     end
-  end
 
-  d = queue_details(c)
-
-  if d[:adds] > limit
-    logger.info 'Work limit reached'
-    break
+    sleep 60
   end
-  sleep 60
+else
+  logger.info "Adding #{limit} new items"
+
+  limit.times do
+    t = f + STEP
+    c.add(QUEUE_NAME, "#{f} #{t}")
+    f = t + 1
+  end
 end
